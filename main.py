5fbee9c9fe3c5ce6711bc17925f38eaa78b0a93d
lucaskf1996 <email@example.com>
2022-02-23 19:20:08 -0300

    clean up

--- main.py
+++ main.py
@@ -79,31 +79,5 @@ class Parser:
         Parser.tokens = Tokenizer(code)
         return(Parser.parseExpression())
 
-def calculadora(eq):
-    index = len(eq)
-    resultado = 0
-    prevNum = 0
-    sinal = 0
-    for i in range(index):
-        if eq[i] == "+":
-            if sinal == 0:
-                resultado += int(eq[prevNum:i])
-            else:
-                resultado -= int(eq[prevNum:i])
-            sinal = 0
-            prevNum = i+1
-        if eq[i] == "-":
-            if sinal == 0:
-                resultado += int(eq[prevNum:i])
-            else:
-                resultado -= int(eq[prevNum:i])
-            sinal = 1
-            prevNum = i+1
-    if sinal == 0:
-        resultado += int(eq[prevNum:])
-    else:
-        resultado -= int(eq[prevNum:])
-    print(resultado)
-
 if __name__ == "__main__":
     print(Parser.run(sys.argv[1]))    
