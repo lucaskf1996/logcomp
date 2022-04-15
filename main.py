06b04c6d3911a2f9d68d8fd53c893218606f2145
lucaskf1996 <email@example.com>
2022-04-15 15:18:04 -0300

    v2.1.0

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import re
 
 class Node():
-    def Evaluate():
+    def Evaluate(self, symbolTable):
         pass
 
 class BinOp(Node):
@@ -16,15 +16,15 @@ class BinOp(Node):
         self.children[1].__repr__()
         print(" ) ")
 
-    def Evaluate(self):
+    def Evaluate(self, symbolTable):
         if self.value == "PLUS":
-            return self.children[0].Evaluate() + self.children[1].Evaluate()
+            return self.children[0].Evaluate(symbolTable) + self.children[1].Evaluate(symbolTable)
         elif self.value == "MINUS":
-            return self.children[0].Evaluate() - self.children[1].Evaluate()
+            return self.children[0].Evaluate(symbolTable) - self.children[1].Evaluate(symbolTable)
         elif self.value == "MULT":
-            return self.children[0].Evaluate() * self.children[1].Evaluate()
+            return self.children[0].Evaluate(symbolTable) * self.children[1].Evaluate(symbolTable)
         elif self.value == "DIV":
-            return self.children[0].Evaluate() // self.children[1].Evaluate()
+            return self.children[0].Evaluate(symbolTable) // self.children[1].Evaluate(symbolTable)
         else:
             raise Exception("Evaluate Error")
 
@@ -38,11 +38,11 @@ class UnOp(Node):
         self.children.__repr__()
         print(" ) ")
 
-    def Evaluate(self):
+    def Evaluate(self, symbolTable):
         if self.value == "PLUS":
-            return self.children.Evaluate()
+            return self.children.Evaluate(symbolTable)
         elif self.value == "MINUS":
-            return -self.children.Evaluate()
+            return -self.children.Evaluate(symbolTable)
         else:
             raise Exception("Evaluate Error")
 
@@ -53,45 +53,109 @@ class IntVal(Node):
     def __repr__(self):
         print(int(self.value), " ")
 
-    def Evaluate(self):
+    def Evaluate(self, symbolTable):
         if self.value.isnumeric():
-            # print(self.value)
             return int(self.value)
         else:
             raise Exception("Evaluate Error")
 
 class NoOp(Node):
-    def __init__(self, value, children):
-        self.value = value
-        self.children = children    
+    def __init__(self):
+        self.value = None
+        self.children = None
 
     def __repr__(self):
-        print(self.value, " children -> ")
-        self.children.__repr__()
-        self.children.__repr__()
+        print("No Children")
 
-    def Evaluate(self):
-        if self.value.isnumeric():
-            return int(self.value)
-        else:
-            raise Exception("Evaluate Error")
+    def Evaluate(self, symbolTable):
+        return
+
+class IdOp(Node):
+    def __init__(self, value):
+        self.value = value
+    
+    def __repr__(self):
+        print(self.value, " ")
+    
+    def Evaluate(self, symbolTable):
+        return symbolTable.getValue(self.value)
+
+class Block(Node):
+    def __init__(self):
+        self.children = []
+
+    def __repr__(self):
+        print("Block children -> ( ")
+        for child in self.children:
+            child.__repr__()
+        print(" ) ")
+
+    def addChild(self, child):
+        self.children.append(child)
+
+    def Evaluate(self, symbolTable):
+        for child in self.children:
+            child.Evaluate(symbolTable)
+
+class PrintOp(Node):
+    def __init__(self, child):
+        self.child = child
+    
+    def __repr__(self):
+        print(self.value, " child -> ( ")
+        self.child.__repr__()
+        print(" ) ")
+
+    def Evaluate(self, symbolTable):
+        print(self.child.Evaluate(symbolTable))
+
+class AssignOp(Node):
+    def __init__(self, children, value):
+        self.children = children
+        self.value = value
+
+    def __repr__(self):
+        print(self.value, " children ->( ")
+        self.children[0].__repr__()
+        self.children[1].__repr__()
+        print(" ) ")
+
+    def Evaluate(self, symbolTable):
+        symbolTable.setValue(self.children[0].value, self.children[1].Evaluate(symbolTable))
+        return
 
 class Token:
     def __init__(self, tokenType, tokenValue):
-        self.tokenType = tokenType #str
-        self.tokenValue = tokenValue #int
+        self.tokenType = tokenType 
+        self.tokenValue = tokenValue 
 
 class Tokenizer:
 
     def __init__(self, origin):
-        self.origin = origin #str
-        self.position = 0 #int
-        self.actual = Token(None, None) #Token
+        self.origin = origin 
+        self.position = 0 
+        self.actual = Token(None, None) 
 
     def selectNext(self):
         num=None
         while(len(self.origin)>self.position):
-            if(self.origin[self.position].isnumeric()):
+            if(self.origin[self.position] == "\n"):
+                self.position+=1
+                continue
+            elif(self.origin[self.position].isalpha()):
+                alpha = self.origin[self.position]
+                self.position+=1
+                while(self.position != len(self.origin)):
+                    if(self.origin[self.position].isnumeric() or self.origin[self.position].isalpha() or self.origin[self.position] == "_"):
+                        alpha = alpha + self.origin[self.position]
+                        self.position+=1
+                    else:
+                        if(alpha == "print"):
+                            self.actual = Token("PRINT", 0)
+                            return
+                        self.actual = Token("ID", alpha)
+                        return
+            elif(self.origin[self.position].isnumeric()):
                 num = self.origin[self.position]
                 self.position+=1
                 while(self.position != len(self.origin)):
@@ -120,23 +184,59 @@ class Tokenizer:
                 self.position+=1
                 return
             elif(self.origin[self.position] == "("):
-                self.actual = Token("OB", 0)
+                self.actual = Token("OP", 0)
                 self.position+=1
                 return
             elif(self.origin[self.position] == ")"):
-                self.actual = Token("CB", 0)
+                self.actual = Token("CP", 0)
+                self.position+=1
+                return
+            elif(self.origin[self.position] == "{"):
+                self.actual = Token("OBLOCK", 0)
+                self.position+=1
+                return
+            elif(self.origin[self.position] == "}"):
+                self.actual = Token("CBLOCK", 0)
+                self.position+=1
+                return
+            elif(self.origin[self.position] == "="):
+                self.actual = Token("EQUAL", 0)
+                self.position+=1
+                return
+            elif(self.origin[self.position] == ";"):
+                self.actual = Token("SC", 0)
                 self.position+=1
                 return
             elif(self.origin[self.position] == " "):
                 self.position+=1
                 continue
+            elif(self.origin[self.position] == "\n"):
+                self.position+=1
+                continue
             else:
+                # print(self.origin[self.position])
                 raise Exception("Invalid char")
         self.actual = Token("EOF", 0)
         return
         
+class SymbolTable:
+    def __init__(self):
+        self.table = {}
+    
+    def getValue(self, identifier):
+        try:
+            return self.table[identifier]
+        except:
+            raise Exception("variable is not found")
+    
+    def setValue(self, identifier, value):
+        # try:
+        self.table[identifier] = value
+        # except:
+        #     raise Exception("variable is not found")
 
 class Parser:
+
     tokens = None
 
     def Factor():
@@ -144,21 +244,24 @@ class Parser:
         if(Parser.tokens.actual.tokenType == "NUM"):
             node = IntVal(Parser.tokens.actual.tokenValue)
             Parser.tokens.selectNext()
+        elif(Parser.tokens.actual.tokenType == "ID"):
+            node = IdOp(Parser.tokens.actual.tokenValue)
+            Parser.tokens.selectNext()
         elif(Parser.tokens.actual.tokenType == "MINUS"):
             node = UnOp(Parser.tokens.actual.tokenType, Parser.Factor())
         elif(Parser.tokens.actual.tokenType == "PLUS"):
             node = UnOp(Parser.tokens.actual.tokenType, Parser.Factor())
-        elif(Parser.tokens.actual.tokenType == "OB"):
+        elif(Parser.tokens.actual.tokenType == "OP"):
             node = Parser.parseExpression()
-            if(Parser.tokens.actual.tokenType == "CB"):
+            if(Parser.tokens.actual.tokenType == "CP"):
                 Parser.tokens.selectNext()
                 return node
             else:
                 raise Exception("invalid sequence2.1")
         else:
+            # print(Parser.tokens.actual.tokenType)
             raise Exception("invalid sequence2.2")
             
-        # print(Parser.tokens.actual.tokenType)
         return node
 
     def parseTerm():
@@ -180,23 +283,66 @@ class Parser:
                 node = BinOp(Parser.tokens.actual.tokenType, [node, Parser.parseTerm()])
         return node
 
-    
+    def parseBlock():
+        Parser.tokens.selectNext()
+        node = Block()
+        if(Parser.tokens.actual.tokenType == "OBLOCK"):
+            Parser.tokens.selectNext()
+            # print(Parser.tokens.actual.tokenType)
+            while(Parser.tokens.actual.tokenType != "CBLOCK"):
+                if(Parser.tokens.actual.tokenType == "EOF"):
+                    raise Exception("Block not closed")
+                node.addChild(Parser.parseStatement())
+                Parser.tokens.selectNext()
+            Parser.tokens.selectNext()
+        else:
+            raise Exception("Block not opened")
+        return node
+
+    def parseStatement():
+        # print(Parser.tokens.actual.tokenType)
+        if(Parser.tokens.actual.tokenType == "SC"):
+            node = NoOp()
+            return node
+        elif(Parser.tokens.actual.tokenType == "ID"):
+            # print(Parser.tokens.actual.tokenType)
+            node = IdOp(Parser.tokens.actual.tokenValue)
+            Parser.tokens.selectNext()
+            if(Parser.tokens.actual.tokenType == "EQUAL"):
+                node = AssignOp([node, Parser.parseExpression()], "=")
+                # Parser.tokens.selectNext()
+                if(Parser.tokens.actual.tokenType == "SC"):
+                    # print(Parser.tokens.actual.tokenType)
+                    # node.__repr__()
+                    return node
+                else:
+                    print(Parser.tokens.actual.tokenType)
+                    raise Exception("missing semi colon")
+            else:
+                raise Exception("invalid sequence")
+        elif(Parser.tokens.actual.tokenType == "PRINT"):
+            node = PrintOp(Parser.parseExpression())
+            if(Parser.tokens.actual.tokenType == "SC"):
+                return node
+            else:
+                raise Exception("missing semi colon")
+        else:
+            raise Exception("missing semi colon")
 
     def code_cleanup(code):
         return(re.sub(r'/[*](.*?)[*]/',"", code))
                     
     def run(code):
+        Parser.symbolTable = SymbolTable()
         Parser.tokens = Tokenizer(Parser.code_cleanup(code))
-        root = Parser.parseExpression()
-        # root.__repr__()
-        # print(Parser.tokens.actual.tokenType)
+        root = Parser.parseBlock()
         if(Parser.tokens.actual.tokenType == "EOF"):
-            return root.Evaluate()
+            return root.Evaluate(Parser.symbolTable)
         else:
             raise Exception("invalid sequence")
 
 if __name__ == "__main__":
     with open(sys.argv[1], 'r') as f:
         contents = f.read()
-    print(Parser.run(contents))
+    Parser.run(contents)
     # print(Parser.run(sys.argv[1]))
